f7fbda86fbd143cc19a6ac2fdbd98d92319e3230
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-06-23 09:00:06 -0700

    fix condition if empty list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -32,7 +32,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *ptr, *back, *point;
 
-	if (!*list || !(*list)->next)
+	if (!list || !(*list)->next)
 		return;
 	ptr = (*list)->next;
 	while (ptr)
