e5fe831925f5d4bc4fffdb86f79550c1311486bf
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-06-25 18:24:00 -0700

    change function algorith to use shift down method

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -16,24 +16,32 @@ void swap_elements(int *first, int *second)
 	*second = temp;
 }
 
-void heaping(int *array, int num, size_t size)
-{
-	int rt, lt, i = num;
+/**
+ * heaping - max heap tree
+ * @array: pointer to array
+ * @start: start comparing index
+ * @num: end of current tree
+ * @size: full size of array
+ * Return: nothing
+ */
 
-	for (; i >= 0; i--)
+void heaping(int *array, int start, int num, size_t size)
+{
+	int p = start, leaf;
+
+	while (p * 2 + 1 <= num)
 	{
-		lt = (i * 2) + 1;
-		rt = (i * 2) + 2;
-		if (lt <= num && array[lt] > array[i])
+		leaf = p * 2 + 1;
+		if (leaf + 1 <= num && array[leaf] < array[leaf + 1])
+			leaf++;
+		if (array[p] < array[leaf])
 		{
-			swap_elements(&array[lt], &array[i]);
+			swap_elements(&array[leaf], &array[p]);
 			print_array(array, size);
+			p = leaf;
 		}
-		if (rt <= num && array[rt] > array[i])
-		{
-			swap_elements(&array[rt], &array[i]);
-			print_array(array, size);
-		}
+		else
+			return;
 	}
 }
 
@@ -51,10 +59,12 @@ void heap_sort(int *array, size_t size)
 
 	if (size < 2)
 		return;
+	for (i = size / 2 - 1; i >= 0; i--)
+		heaping(array, i, (int)size - 1, size);
 	for (i = size - 1; i > 0; i--)
 	{
-		heaping(array, i, size);
 		swap_elements(&array[0], &array[i]);
 		print_array(array, size);
+		heaping(array, 0, i - 1, size);
 	}
 }
